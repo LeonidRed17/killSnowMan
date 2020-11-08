d4663b0b4ca26d004881497b22d5b8783e6b78c0
leonid666 <54948190+leonid666@users.noreply.github.com>
2020-11-08 05:13:58 +0300

    DLC For pre orders customers (ver. 0.1)
    +UI upgrades
    +new battle mechanics.
    and more..

--- script.js
+++ script.js
@@ -1,33 +1,87 @@
-function strategyGame(){
+function strategyGame() {
+    /*
     alert('Да здравствует новое время!! в новой игре!')
     alert('Вас приветствует сыр-бор продукшен и святозор! Мы выпускаем игры!!!')
     alert('При производстве игры наш двор убирала снегоуборочная техника!! спасибо!')
     alert('Добро пожаловать в "Убийство Снежного человека 2"')
     alert('Попробуй пройти её и ты!! и все друзья!!!')
-    function play(){
-        var resultBlock = document.getElementById("result");
-        var playerHP = 100;
-        var playerDamage = 1;
-        var chance = 0;
-        var computerHP = 100;
-        var computerDamage = 0.5;
-        function attackEnemy(){
-            computerHP -= 1;
-            resultBlock.innerText = ('враг атакован -1hp здоровья врага: ' + computerHP);
-            if(computerHP <90){
-                computerHP = 100;
-                chance +=1;
-                resultBlock.innerText = ('Я вылечился, отстань!! держись!!!');
-                if(chance >= 5){
-                    alert('МОжет эта игра тебе не позубам!??');
-                    chance = 0;
-                }
-            }
-        }
+    */
+   
+    function play() {
+        //User interface
+        var attackComputerBtn = document.getElementById('attackenemy');
 
-    addEventListener("click",attackEnemy);
+        var combatLogBlock = document.getElementById('result');
+        var combatLogLaunch = 0;
+        var playerHP = 100;
+        var playerDMG = 1;
+
+        var enemyHealthStatusBar = document.getElementById('enemy-health-status-bar');
+        enemyHealthStatusBar.style.width = '100%';
+        var enemyHealthStatusBarDecrease;
+        var enemyImage = document.getElementById("enemy-image");
+        enemyImage.style.opacity =1;
+
+        var computerHP = 100;
+        var computerDMG = 0.5;
+        function attack() {
+            computerHP -= playerDMG;
+            enemyHealthStatusBarDecrease = parseInt(enemyHealthStatusBar.style.width) - 1;
+            enemyHealthStatusBar.style.width = enemyHealthStatusBarDecrease + '%';
+            enemyImage.style.opacity -= '0.01';
+            
+            if(computerHP == 0){
+                attackComputerBtn.removeEventListener('click',attack);
+            }
+            combatLog();
+            
+        }
+       
+        if(computerHP >0){
+            attackComputerBtn.addEventListener('click',attack);
+        }
+        //Combat Log block
+        function combatLog(){
+            var combatLogElement = document.createElement("p")
+            
+            combatLogElement.textContent = 'Здоровье врага: ' + computerHP;
+           
+            if(combatLogLaunch === 0){
+                combatLogBlock.appendChild(combatLogElement);
+                combatLogLaunch = 1;
+                
+            } else{
+                combatLogBlock.insertBefore(combatLogElement,combatLogBlock.firstChild);
+                if(computerHP == 95){
+                    combatLogElement.textContent = 'Ай! Больно';
+                }
+                if(computerHP == 85){
+                    combatLogElement.textContent = 'Что ж вы делаете то!';
+                }
+                if(computerHP == 65){
+                    combatLogElement.textContent = 'Ну будьте же вы людьми!!';
+                }
+                if(computerHP == 50){
+                    combatLogElement.textContent = 'Помогите!!';
+                }
+                if(computerHP == 10){
+                    combatLogElement.textContent = 'Спасите!';
+                }
+                if(computerHP == 0){
+                    combatLogElement.textContent = 'Вы убили снежного человека вам должно быть стыдно. Игра окончена для вас. Пришлите деньги на номер телефона по СМС..';
+                }
+
+            }
+            combatLogLaunch++;
+        }
+        
+        function computerStatusBar(){
+            //
+        }
     }
+   
     play();
 }
+
 
 strategyGame();
